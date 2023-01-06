cdf0d71a2939e26022df85f969cf2dfc78c8ab4b
inho93 <qazwroy@naver.com>
2023-01-07 00:45:15 +0900

    2023.01.07 Spring boot 시작

---
+++ inho/src/main/java/com/inho/kim/InhoApplication.java
@@ -0,0 +1,13 @@
+package com.inho.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhoApplication.class, args);
+	}
+
+}
---
+++ inho/src/test/java/com/inho/kim/InhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inho.kim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
